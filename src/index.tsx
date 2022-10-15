d42adc434c63aa1d9464f60b93cccb9d8f802dea
Michael Puckett <michaelcpuckett@gmail.com>
2022-10-15 18:11:34 -0400

    build

--- src/index.tsx
+++ src/index.tsx
@@ -26,7 +26,7 @@ const serviceAccount: ServiceAccount = JSON.parse(decodeURIComponent(envServiceA
 
   const authenticationService = new FirebaseAuthentication(serviceAccount, 'pickpuck-com');
   const databaseService = await new MongoDatabaseService().connect({
-    mongoClientUrl: 'mongodb://localhost:27017',
+    mongoClientUrl: process.env.AP_MONGO_CLIENT_URL ?? 'mongodb://localhost:27017',
     dbName: 'michaelpuckett-engineer',
   });
   const deliveryService = new DeliveryService(databaseService);
