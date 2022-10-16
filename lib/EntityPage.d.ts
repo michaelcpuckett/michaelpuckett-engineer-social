67bdfb876b96dc6b3e577d31632f863b719da6cc
Michael Puckett <michaelcpuckett@gmail.com>
2022-10-15 22:44:29 -0400

    update deps

--- lib/EntityPage.d.ts
+++ lib/EntityPage.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="react" />
 import { AP } from 'activitypub-core-types';
-export declare function EntityPage({ entity, actor }: {
+export declare function EntityPage({ entity, actor: user }: {
     entity: AP.Entity;
     actor?: AP.Actor;
 }): JSX.Element;
