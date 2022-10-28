cc864bb38ea5258e12d1391cf0a43b2935a8de40
Michael Puckett <michaelcpuckett@gmail.com>
2022-10-28 11:55:43 -0400

    attempt with new API

--- src/index.tsx
+++ src/index.tsx
@@ -8,13 +8,13 @@ import { LoginPage } from './LoginPage';
 import { DashboardPage } from './DashboardPage';
 import { EntityPage } from './EntityPage';
 import { renderToString } from 'react-dom/server';
-import { MongoDatabaseService } from 'activitypub-core-mongodb';
-import { FirebaseAuthentication } from 'activitypub-core-firebase-authentication';
-import { FtpStorage } from 'activitypub-core-ftp-storage';
-import { DeliveryService } from 'activitypub-core-delivery';
+import { MongoDatabaseAdapter } from 'activitypub-core-mongodb';
+import { FirebaseAuthenticationAdapter } from 'activitypub-core-firebase-authentication';
+import { FtpStorageAdapter } from 'activitypub-core-ftp-storage';
+import { DeliveryAdapter } from 'activitypub-core-delivery';
 import { ServiceAccount } from 'firebase-admin';
 import { ServerResponse, IncomingMessage } from 'http';
-import { foafPlugin } from 'activitypub-core-plugin-foaf';
+import { FoafPlugin } from 'activitypub-core-plugin-foaf';
 
 const envServiceAccount = process.env.AP_SERVICE_ACCOUNT;
 
@@ -22,89 +22,100 @@ if (!envServiceAccount) {
   throw new Error('Bad Service Account.');
 }
 
-const serviceAccount: ServiceAccount = JSON.parse(decodeURIComponent(envServiceAccount));
+const firebaseServiceAccount: ServiceAccount = JSON.parse(decodeURIComponent(envServiceAccount));
 
 (async () => {
   const app = express();
   app.use(express.static('static/'));
 
-  const authenticationService = new FirebaseAuthentication(serviceAccount, 'pickpuck-com');
-  const databaseService = await new MongoDatabaseService().connect({
-    mongoClientUrl: process.env.AP_MONGO_CLIENT_URL ?? 'mongodb://localhost:27017',
-    dbName: 'puckett-contact',
-  });
-  const deliveryService = new DeliveryService(databaseService);
-  const storageService = new FtpStorage(JSON.parse(decodeURIComponent(process.env.AP_FTP_CONFIG)), '/uploads');
+  const firebaseAuthenticationAdapter =
+    new FirebaseAuthenticationAdapter(
+      firebaseServiceAccount,
+      'pickpuck-com'
+    );
+
+  const mongoDatabaseAdapter =
+    await new MongoDatabaseAdapter().connect({
+      mongoClientUrl: process.env.AP_MONGO_CLIENT_URL ?? 'mongodb://localhost:27017',
+      dbName: 'puckett-contact',
+    });
+
+  const defaultDeliveryAdapter =
+    new DeliveryAdapter({
+      adapters: {
+        database: mongoDatabaseAdapter,
+      },
+    });
+
+  const ftpStorageAdapter =
+    new FtpStorageAdapter(
+      JSON.parse(decodeURIComponent(process.env.AP_FTP_CONFIG)),
+      '/uploads'
+    );
+  
+  const foafPlugin = FoafPlugin(); // TODO: Make callable with `new`.
+  
+  const renderLoginPage = async () => {
+    return `
+      <!doctype html>
+      ${renderToString(
+        <LoginPage />
+      )}`;
+  };
+
+  const renderHomePage = async ({ actor }) => {
+    return `
+      <!doctype html>
+      ${renderToString(
+        <DashboardPage actor={actor} />
+      )}
+    `;
+  };
+
+  const renderEntityPage = async ({ entity, actor }) => {
+    return `
+      <!doctype html>
+      ${renderToString(
+        <EntityPage
+          entity={entity}
+          actor={actor}
+        />
+      )}
+    `;
+  };
 
   app.use(
-    activityPub(
-      {
-        renderLogin: async () => {
-          return `
-            <!doctype html>
-            ${renderToString(<LoginPage />)}`;
-        },
-        renderEntity: async ({ entity, actor }) => {
-          return `
-            <!doctype html>
-            ${renderToString(<EntityPage entity={entity} actor={actor} />)}
-         `;
-        },
-        renderHome: async ({ actor }) => {
-          return `
-            <!doctype html>
-            ${renderToString(<DashboardPage actor={actor} />
-          )}
-        `;
-        },
+    activityPub({
+      pages: {
+        login: renderLoginPage,
+        home: renderHomePage,
+        entity: renderEntityPage,
       },
-      {
-        authenticationService,
-        databaseService,
-        deliveryService,
-        storageService,
-        plugins: [
-          foafPlugin({
-            newPerson: JSON.parse(JSON.stringify({
-              "type": ["Actor", "foaf:Person"], // TODO
-              "foaf:name": "Michael Puckett",
-              "foaf:basedNear": "Louisville, KY, USA",
-              "foaf:birthday": "06-17",
-              "foaf:img": "https://michaelpuckett.engineer/avatar.png",
-              "foaf:logo": "https://michaelpuckett.engineer/favicon.svg",
-              "foaf:mbox": "mailto:michael@puckett.contact",
-              "foaf:homepage": "https://michaelpuckett.engineer",
-              "foaf:weblog": "https://puckett.contact/actor/michael",
-              "foaf:nick": "michaelcpuckett",
-              "foaf:holdsAccount": [{
-                "@type": "foaf:OnlineAccount",
-                "foaf:accountServiceHomepage": "https://github.com",
-                "foaf:accountName": "michaelcpuckett",
-              }, {
-                "@type": "foaf:OnlineAccount",
-                "foaf:accountServiceHomepage": "https://linkedin.com",
-                "foaf:accountName": "michaelcpuckett",
-              }],
-              "foaf:currentProject": "https://github.com/michaelcpuckett/activitypub-core",
-              "foaf:interest": [
-                "https://dbpedia.org/resource/Web_accessibility"
-              ],
-              "foaf:workplaceHomepage": "https://google.com",
-              "foaf:schoolHomepage": "https://wku.edu",
-            }))
-          }),
-        ]
+
+      adapters: {
+        authentication: firebaseAuthenticationAdapter,
+        database: mongoDatabaseAdapter,
+        delivery: defaultDeliveryAdapter,
+        storage: ftpStorageAdapter,
       },
-    ),
+
+      plugins: [
+        foafPlugin
+      ]
+    }),
   );
 
   app.get('/', (req: IncomingMessage, res: ServerResponse) => {
+    const indexPage = `
+      <!doctype html>
+      ${renderToString(
+        <IndexPage />
+      )}
+    `;
+
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html')
-    res.write(`
-      <!doctype html>
-      ${renderToString(<IndexPage />)}
-    `);
+    res.write(indexPage);
     res.end();
   });
 
