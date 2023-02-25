6d43cd521be9b7b704f49ae39415cf01642c9320
Michael Puckett <michael@puckett.contact>
2023-02-25 12:45:22 -0500

    x

--- index.js
+++ index.js
@@ -83,24 +83,6 @@ const port = process.env.PORT ?? 3000;
     });
 
     const foodData = [{
-      type: 'Cuisines',
-      items: [{
-        name: 'American food',
-        imageUrl: 'american.png',
-      }, {
-        name: 'Mexican food',
-        imageUrl: 'mexican.png',
-      }, {
-        name: 'Chinese food',
-        imageUrl: 'chinese.png'
-      }, {
-        name: 'Sushi',
-        imageUrl: 'sushi.png',
-      }, {
-        name: 'Indian food',
-        imageUrl: 'indian.png',
-      }]
-    }, {
       type: 'Protein',
       items: [{
         name: 'Chicken',
@@ -258,6 +240,24 @@ const port = process.env.PORT ?? 3000;
       }, {
         name: 'Cereal',
         imageUrl: 'cereal.png',
+      }]
+    }, {
+      type: 'Cuisines',
+      items: [{
+        name: 'American food',
+        imageUrl: 'american.png',
+      }, {
+        name: 'Mexican food',
+        imageUrl: 'mexican.png',
+      }, {
+        name: 'Chinese food',
+        imageUrl: 'chinese.png'
+      }, {
+        name: 'Sushi',
+        imageUrl: 'sushi.png',
+      }, {
+        name: 'Indian food',
+        imageUrl: 'indian.png',
       }]
     }];
 
